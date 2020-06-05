7491d85398594ead75e2ac9be850e478dd5a92f6
gabirum <gabirum@users.noreply.github.com>
2020-06-05 16:54:19 -0400

    Player phase two

--- web/src/components/Player/index.tsx
+++ web/src/components/Player/index.tsx
@@ -1,30 +1,54 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import { useSong } from '../../hooks/song';
 import { PlayerDiv, PlayerContainer, PlayerPart} from './styles';
-import {FiPlayCircle} from 'react-icons/fi';
+import {FiPlayCircle, FiPauseCircle} from 'react-icons/fi';
 
 const Player = () => {
-  const songContext = useSong()
+  const songContext = useSong();
 
   const [song, setSong] = useState('');
+  const [play, setPlay] = useState(false);
+  const [audio, setAudio] = useState<HTMLAudioElement | null>();
+  const [time, setTime] = useState(0);
+  const [maxTime, setMaxTime] = useState(0);
 
   useEffect(() => {
     setSong(songContext.song.song_url);
+    setMaxTime(audio?.duration ? audio.duration:0);
+  }, [song, songContext.song]);
 
-  }, [song, songContext.song])
+  function handlePlay() {
+    if (play) {
+      setPlay(!play);
+      audio?.pause();
+    } else {
+      setPlay(!play);
+      audio?.play();
+    }
+  }
+
+  function handleTimeUpdate() {
+
+  }
 
   return (
   <PlayerDiv>
     <PlayerContainer>
-      <audio src={song} autoPlay/>
+      <audio ref={a => setAudio(a)} src={song} autoPlay onPlay={() => setPlay(true)} onTimeUpdate={handleTimeUpdate}/>
       <PlayerPart>
-        <p className="musicName">
-          
-        </p>
-        <button>
-          <FiPlayCircle size='20' color='#fff'/>
+        <div>
+          <p className="musicName">
+            {songContext.song.song}
+          </p>
+          <p className="musicAuthor">
+            {songContext.song.author}
+          </p>
+        </div>
+        <button onClick={handlePlay}>
+          {play ? <FiPauseCircle size='24' color='#fff'/>:<FiPlayCircle size='24' color='#fff'/>}
         </button>
+        <progress value={time} max={maxTime} />
       </PlayerPart>
     </PlayerContainer>
   </PlayerDiv>
